9236d263b4ed63a3b8c82ff387b0f2ecee66b24e
Phil Snyder <subfivemile@gmail.com>
2017-12-15 12:09:22 -0800

    set annotations, provenance, PEP 8 compliance, dockerfile cleanup

--- down_sample_re_count.py
+++ down_sample_re_count.py
@@ -1,47 +1,61 @@
-import synapseclient, os, sys
+from __future__ import print_function
+import synapseclient
+import os
+import sys
 
-def pull_data(synId):
-    #1. pull file into local directory
-    #2. syn = synapseclient.Synapse()
-    syn = synapseclient.Synapse()
-    syn.login(os.environ['synapseUser'],os.environ['synapsePassword'])
-    print("logged in")
-    entity = syn.get(synId,downloadLocation = './')
+
+def pull_data(synId, syn):
+    # 1. pull file into local directory
+    # 2. syn = synapseclient.Synapse()
+    entity = syn.get(synId, downloadLocation='./')
     print("file downloaded")
-    bar = entity.path
-    return(bar)
-    #get file name from the entity object
-    #return...
+    return entity.path, entity.annotations
+
 
 def run_samtools(fileName):
     print("running samtools")
-    str = ''.join(['samtools view -b -s 0.5 ',fileName,' > ',fileName,'_new.bam'])
+    str = ''.join(['samtools view -b -s 0.5 ', fileName,
+                   ' > ', fileName, '_new.bam'])
     os.system(str)
     print("samtools run")
-    str2 = ''.join([fileName,'_new.bam'])
-    return(str2)
+    str2 = ''.join([fileName, '_new.bam'])
+    return str2
 
-def run_htseq(fileName,gtfFileName):
-    str = ''.join(['htseq-count -f bam ',fileName,' ',gtfFileName,' > ',fileName,'_counts.txt'])
+
+def run_htseq(fileName, gtfFileName):
+    str = ''.join(['htseq-count -f bam ', fileName, ' ', gtfFileName,
+                   ' > ', fileName, '_counts.txt'])
     print("running htseq")
     os.system(str)
     print("htseq run")
-    str2 = ''.join([fileName,'_counts.txt'])
-    return(str2)
+    str2 = ''.join([fileName, '_counts.txt'])
+    return str2
 
-def push_to_synapse(fileName,uploadLocation):
-    syn = synapseclient.Synapse()
-    syn.login(os.environ['synapseUser'],os.environ['synapsePassword'])
-    entity = synapseclient.File(fileName,parentId = uploadLocation)
+
+def push_to_synapse(fileName, uploadLocation, synId, annotations, syn):
+    annotations['fileFormat'] = ['txt']
+    entity = synapseclient.File(
+            fileName,
+            parentId=uploadLocation,
+            used=synId,
+            executed='https://github.com/Sage-Bionetworks'
+                     '/DockerRNAseq/blob/master/Dockerfile')
     entity = syn.store(entity)
     print("uploaded to synapse")
+
 
 if __name__ == '__main__':
     synId = sys.argv[1]
     uploadLocation = sys.argv[2]
-    print(synId)
-    print(uploadLocation)
-    foo = pull_data(synId)
-    foo = run_samtools(foo)
-    foo = run_htseq(foo,"/app/gencode.v24.primary_assembly.annotation.gtf")
-    foo = push_to_synapse(foo,uploadLocation)
+    print("input", synId)
+    print("output location", uploadLocation)
+    syn = synapseclient.login(
+            os.environ['synapseUser'],
+            os.environ['synapsePassword'])
+    print("logged in")
+    local_path, annotations = pull_data(synId, syn)
+    sam_output = run_samtools(local_path)
+    htseq_output = run_htseq(
+            sam_output,
+            "/app/gencode.v24.primary_assembly.annotation.gtf")
+    push_to_synapse(htseq_output, uploadLocation, synId, annotations, syn)
